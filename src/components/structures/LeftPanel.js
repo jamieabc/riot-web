71c621a8f4e35c5c684ccebd73232cdcf717dc4c
Aaron Chang <aaron.chang@vpon.com>
2017-05-14 12:33:31 +0800

    add hotkey to switch between left panel items

--- src/components/structures/LeftPanel.js
+++ src/components/structures/LeftPanel.js
@@ -19,7 +19,7 @@ limitations under the License.
 var React = require('react');
 var DragDropContext = require('react-dnd').DragDropContext;
 var HTML5Backend = require('react-dnd-html5-backend');
-var sdk = require('matrix-react-sdk')
+var sdk = require('matrix-react-sdk');
 var dis = require('matrix-react-sdk/lib/dispatcher');
 
 var VectorConferenceHandler = require('../../VectorConferenceHandler');
@@ -30,14 +30,18 @@ var LeftPanel = React.createClass({
 
     propTypes: {
         collapsed: React.PropTypes.bool.isRequired,
-        teamToken: React.PropTypes.string,
+        teamToken: React.PropTypes.string
     },
 
     getInitialState: function() {
         return {
             showCallElement: null,
-            searchFilter: '',
+            searchFilter: ''
         };
+    },
+
+    componentWillMount: function() {
+      this.focusedElement = null;
     },
 
     componentDidMount: function() {
@@ -50,6 +54,95 @@ var LeftPanel = React.createClass({
 
     componentWillUnmount: function() {
         dis.unregister(this.dispatcherRef);
+    },
+
+    onFocus: function(ev) {
+      this.focusedElement = ev.target;
+    },
+
+    onBlur: function(ev) {
+      this.focusedElement = null;
+    },
+
+    onKeyDown: function(ev) {
+      if(!this.focusedElement) return;
+      let handled = false;
+      const ctrlOnly = ev.ctrlKey && !ev.metaKey && !ev.altKey && !ev.shiftKey;
+
+      switch(ev.keyCode) {
+      case 38:                  // key up
+        this.onChaneFocusItem(true);
+        handled = true;
+        break;
+      case 40:                  // key down
+        this.onChaneFocusItem(false);
+        handled = true;
+        break;
+      case 78:                  // ctrl-n
+        if (ctrlOnly) {
+          this.onChaneFocusItem(false);
+          handled = true;
+        }
+        break;
+      case 80:                  // ctrl-p
+        if (ctrlOnly) {
+          this.onChaneFocusItem(true);
+          handled = true;
+        }
+        break;
+      }
+
+      if (handled) {
+        ev.stopPropagation();
+        ev.preventDefault();
+      }
+    },
+
+    onChaneFocusItem: function(up) {
+      var current = this.focusedElement;
+
+      var descending = false;
+      var classes;
+
+      do {
+        var child = up ? current.lastElementChild : current.firstElementChild;
+        var sibling = up ? current.previousElementSibling : current.nextElementSibling;
+
+        if (descending) {
+          if (child) {
+            current = child;
+          } else if (sibling) {
+            current = sibling;
+          } else {
+            descending = false;
+            current = current.parentElement;
+          }
+        } else {
+          if (sibling) {
+            current = sibling;
+            descending = true;
+          } else {
+            current = current.parentElement;
+          }
+        }
+
+        if (current) {
+          classes = current.classList;
+          if (classes.contains("mx_LeftPanel")) { // top list
+            current = up ? current.lastElementChild : current.firstElementChild;
+            descending = true;
+          }
+        }
+      } while(current && !(
+        classes.contains("mx_RoomTile") ||
+        classes.contains("mx_SearchBox_search") ||
+        classes.contains("mx_RoomSubList_ellipsis")));
+
+      if (current) {
+        current.focus();
+        this.focusedElement = current;
+        this.focusedDescending = descending;
+      }
     },
 
     onAction: function(payload) {
@@ -120,17 +213,23 @@ var LeftPanel = React.createClass({
         }
 
         return (
-            <aside className={classes} style={{ opacity: this.props.opacity }}>
-                <SearchBox collapsed={ this.props.collapsed } onSearch={ this.onSearch } />
-                { collapseButton }
-                { callPreview }
-                <RoomList
-                    selectedRoom={this.props.selectedRoom}
-                    collapsed={this.props.collapsed}
-                    searchFilter={this.state.searchFilter}
-                    ConferenceHandler={VectorConferenceHandler} />
-                <BottomLeftMenu collapsed={this.props.collapsed} teamToken={this.props.teamToken}/>
-            </aside>
+          <aside
+            className={classes}
+            style={{ opacity: this.props.opacity }}
+            onKeyDown={ this.onKeyDown }
+            onFocus={ this.onFocus }
+            onBlur={ this.onBlur }
+          >
+            <SearchBox collapsed={ this.props.collapsed } onSearch={ this.onSearch } />
+              { collapseButton }
+              { callPreview }
+            <RoomList
+              selectedRoom={this.props.selectedRoom}
+              collapsed={this.props.collapsed}
+              searchFilter={this.state.searchFilter}
+              ConferenceHandler={VectorConferenceHandler} />
+            <BottomLeftMenu collapsed={this.props.collapsed} teamToken={this.props.teamToken}/>
+          </aside>
         );
     }
 });
